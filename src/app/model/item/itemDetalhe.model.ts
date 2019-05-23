7acfa5dfdf3bd5f76b78f8a2d1a79607c9c99202
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-05-23 09:47:25 -0300

    First Commit

---
+++ src/app/model/item/itemDetalhe.model.ts
@@ -0,0 +1,9 @@
+import { ItemBase } from './itemBase.model';
+import { ConsultaRegrasFiscais } from './consultaRegrasFisicais.model';
+
+export class ItemDetalhe {
+  public filial: number;
+  public perfil: number;
+  public itens: Array<any> = new Array();
+  public consultaRegrasFiscais: ConsultaRegrasFiscais;
+}
