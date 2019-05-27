44f28e07905bc7d6aef35c98333d44b04c16541a
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-05-27 17:13:28 -0300

    Cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuthInterceptor } from './http-interceptors/auth-interceptor';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ItemModule } from './modules/item.module';
+import { BootstrapModule } from './modules/bootstrap.module';
+import { InterceptorsModule } from './modules/interceptors.module';
+import { SharedModule } from './modules/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -15,11 +16,13 @@ import { ItemModule } from './modules/item.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule,
     ItemModule,
-    FormsModule
+    HttpClientModule,
+    BootstrapModule,
+    InterceptorsModule,
+    SharedModule
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
+  providers: [  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
