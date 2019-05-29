80ff85edaab9445107baa2e4427868166536b3f8
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-05-29 17:47:39 -0300

    Added that missing

--- src/app/modules/bootstrap.module.ts
+++ src/app/modules/bootstrap.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
-import { ModalModule } from 'ngx-bootstrap/modal';
+import { ModalModule, BsModalRef } from 'ngx-bootstrap/modal';
 import { ToastrModule } from 'ngx-toastr';
+import { ModalItemComponent } from '../components/modal-item/modal-item.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
     BsDropdownModule.forRoot(),
     TooltipModule.forRoot(),
     ModalModule.forRoot(),
-    ToastrModule.forRoot()
+    ToastrModule.forRoot(),
+    CommonModule
+  ],
+  declarations: [
+    ModalItemComponent
+  ],
+  entryComponents: [
+    ModalItemComponent
+  ],
+  providers: [
+    BsModalRef
   ]
 })
 export class BootstrapModule { }
