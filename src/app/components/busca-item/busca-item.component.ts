3436e99e1b8f46411c263553a525d3b249424d66
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-05-30 16:37:49 -0300

    Code refactoring

--- src/app/components/busca-item/busca-item.component.ts
+++ src/app/components/busca-item/busca-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpParams } from '@angular/common/http';
-import { ItemResponse } from '../../model/item/itemResponse.model';
 import { ItemResponseDetalhada } from '../../model/item/itemResponseDetalhada.model';
 import { Item } from 'src/app/model/item/item.model';
 import { ItemPost } from 'src/app/model/item/itemPost.model';
@@ -21,7 +20,6 @@ import { ModalItemComponent } from '../modal-item/modal-item.component';
 })
 export class BuscaItemComponent implements OnInit {
 
-  listItemBase: Array<ItemResponse> = new Array<ItemResponse>();
   listItemDetalheResponse: Array<ItemResponseDetalhada> = new Array<ItemResponseDetalhada>();
   listItemEstoqueResponse: Array<ItemResponseEstoque> = new Array<ItemResponseEstoque>();
   listItemPrecoResponse: Array<ItemResponsePreco> = new Array<ItemResponsePreco>();
@@ -29,6 +27,7 @@ export class BuscaItemComponent implements OnInit {
   modalRef: BsModalRef;
 
   ngOnInit() {
+    this.buscaProduto('ibupro')
   }
 
   constructor(public itemService: ItemService,
@@ -38,19 +37,14 @@ export class BuscaItemComponent implements OnInit {
   openModal(item: any) {
     this.modalService.config.animated = true;
     this.modalService.config.ignoreBackdropClick = true;
-    this.modalRef = this.modalService.show(ModalItemComponent, { initialState: { item } });
+    this.modalRef = this.modalService.show(ModalItemComponent, { initialState: { item }, class: 'modal-lg' });
   }
 
-  buscaProduto(nome: string,
-               codigoFilial = 1,
-               maxResult = 40,
-               ordenarRentabilidade = false,
-               ordenarPreco = false) {
+  buscaProduto(nome: string) {
     if (nome.length > 2) {
-      this.itemService.findByName(nome, codigoFilial, maxResult, ordenarRentabilidade, ordenarPreco).subscribe((
+      this.itemService.findByName(nome , 101, 40, false, false).subscribe((
         response: []) => {
-        this.listItemBase = response;
-        this.getDetalhe();
+        this.montaObjetoDoPostDetalhe(response);
       });
     } else {
       this.toastr.toastrConfig.preventDuplicates = true;
@@ -59,14 +53,8 @@ export class BuscaItemComponent implements OnInit {
     }
   }
 
-  addImagemSeNaoExistir(item: ItemResponseDetalhada) {
-    item.dadosImagens = new Array<Imagem>();
-    const imagem = new Imagem();
-    imagem.url = '../../../assets/images/sem-imagem.gif';
-    item.dadosImagens.push(imagem);
-  }
 
-  getDetalhe() {
+  montaObjetoDoPostDetalhe(responseFindByName) {
     const itemDetalhePost: ItemPost = new ItemPost();
     itemDetalhePost.consultaRegrasFiscais = new ConsultaRegrasFiscais();
     itemDetalhePost.consultaRegrasFiscais.pais = 'BR';
@@ -76,15 +64,19 @@ export class BuscaItemComponent implements OnInit {
     itemDetalhePost.filial = 101;
     itemDetalhePost.perfil = 1;
     itemDetalhePost.itens = new Array<Item>();
-    this.listItemBase.map(item => {
+    responseFindByName.map(item => {
       const itemMap: Item = new Item();
       itemMap.codigo = item.codigoItem;
       itemDetalhePost.itens.push(itemMap);
     });
+    responseFindByName = [];
+    this.getDetalhe(itemDetalhePost);
+  }
+
+  getDetalhe(itemDetalhePost) {
     this.itemService.findItemDetalhe(itemDetalhePost).subscribe((
       response: any) => {
       this.listItemDetalheResponse = response.itens;
-      this.listItemBase = new Array<ItemResponse>();
       this.getEstoque();
       this.getPreco();
     });
@@ -92,24 +84,20 @@ export class BuscaItemComponent implements OnInit {
 
   getEstoque() {
     let parametros = new HttpParams();
-    // tslint:disable-next-line: prefer-for-of
-    for (let i = 0; i < this.listItemDetalheResponse.length; i++) {
-      parametros = parametros.append('itens', this.listItemDetalheResponse[i].codigo.toString());
+    for (const item of this.listItemDetalheResponse) {
+      parametros = parametros.append('itens', item.codigo.toString());
     }
-    const options = { params: parametros };
-    this.itemService.findEstoqueByCodigo(options).subscribe((response: any) => {
+    this.itemService.findEstoqueByCodigo({ params: parametros }).subscribe((response: any) => {
       this.listItemEstoqueResponse = response;
     });
   }
 
   getPreco() {
     let parametros = new HttpParams();
-    // tslint:disable-next-line: prefer-for-of
-    for (let i = 0; i < this.listItemDetalheResponse.length; i++) {
-      parametros = parametros.append('item', this.listItemDetalheResponse[i].codigo.toString());
+    for (const item of this.listItemDetalheResponse) {
+      parametros = parametros.append('item', item.codigo.toString());
     }
-    const options = { params: parametros };
-    this.itemService.findPreco(options, 101, 1).subscribe((response: any) => {
+    this.itemService.findPreco({ params: parametros }, 101, 1).subscribe((response: any) => {
       this.listItemPrecoResponse = response;
       this.adicionaPrecoAndEstoqueNaLista();
     });
@@ -117,18 +105,23 @@ export class BuscaItemComponent implements OnInit {
 
   adicionaPrecoAndEstoqueNaLista() {
     this.listItemDetalheResponse.forEach((item, i) => {
-      const estoqueFiltrado = this.listItemEstoqueResponse.find((estoqueFiltro) =>
-        estoqueFiltro.codigoItem === item.codigo
-      );
-      const precoFiltrado = this.listItemPrecoResponse.find((precoFiltro) =>
-        precoFiltro.codigoItem === item.codigo
-      );
-      item.estoque = estoqueFiltrado.estoqueLoja;
-      item.preco = precoFiltrado.preco;
+      item.estoque = this.listItemEstoqueResponse.find((estoqueFiltro) =>
+                    estoqueFiltro.codigoItem === item.codigo
+                    ).estoqueLoja;
+      item.preco = this.listItemPrecoResponse.find((precoFiltro) =>
+                    precoFiltro.codigoItem === item.codigo
+                    ).preco;
 
       if (item.dadosImagens.length === 0) {
         this.addImagemSeNaoExistir(item);
       }
     });
+  }
+
+  addImagemSeNaoExistir(item: ItemResponseDetalhada) {
+    item.dadosImagens = new Array<Imagem>();
+    const imagem = new Imagem();
+    imagem.url = '../../../assets/images/sem-imagem.gif';
+    item.dadosImagens.push(imagem);
   }
 }
