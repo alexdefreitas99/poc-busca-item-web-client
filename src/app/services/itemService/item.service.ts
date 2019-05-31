4b67e408f9dd206a9c6cc5288a38457559e3663a
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-05-31 08:37:03 -0300

    Formated document

--- src/app/services/itemService/item.service.ts
+++ src/app/services/itemService/item.service.ts
@@ -20,25 +20,25 @@ export class ItemService {
              codigoFilial: number,
              maxResult: number,
              ordenarRentabilidade: boolean,
-             ordenarPreco: boolean): Observable<ItemResponse> {
+             ordenarPreco: boolean): Observable<ItemResponse[]> {
     // tslint:disable-next-line: max-line-length
-    return this.http.get<ItemResponse>(`${this.parameterService.getValueFromProperties(Constantes.GRUPO_DIMED_SERVICE)}item/v3/itens/base/autocomplete?nome=${nome}&codigoFilial=${codigoFilial}&maxResult=${maxResult}&ordenarRentabilidade=${ordenarRentabilidade}&ordenarPreco=${ordenarPreco}`);
+    return this.http.get<ItemResponse[]>(`${this.parameterService.getValueFromProperties(Constantes.GRUPO_DIMED_SERVICE)}item/v3/itens/base/autocomplete?nome=${nome}&codigoFilial=${codigoFilial}&maxResult=${maxResult}&ordenarRentabilidade=${ordenarRentabilidade}&ordenarPreco=${ordenarPreco}`);
   }
 
-  findItemDetalhe(itemDetalhe: ItemPost): Observable<ItemResponseDetalhada> {
-    return this.http.post<ItemResponseDetalhada>
+  findItemDetalhe(itemDetalhe: ItemPost): Observable<ItemResponseDetalhada[]> {
+    return this.http.post<ItemResponseDetalhada[]>
       (`${this.parameterService.getValueFromProperties(Constantes.GRUPO_DIMED_SERVICE)}mostruario/v3/itens/detalhe`,
         itemDetalhe);
   }
 
-  findEstoqueByCodigo(params: any): Observable<HttpEvent<ItemResponseEstoque>> {
-    return this.http.get<ItemResponseEstoque>
+  findEstoqueByCodigo(params: any): Observable<HttpEvent<ItemResponseEstoque[]>> {
+    return this.http.get<ItemResponseEstoque[]>
       (`${this.parameterService.getValueFromProperties(Constantes.GRUPO_DIMED_SERVICE)}filial/v1/filiais/101/estoque?`,
         params);
   }
 
-  findPreco(params: any, filial: number, perfil: number): Observable<HttpEvent<ItemResponsePreco>> {
-    return this.http.get<ItemResponsePreco>
+  findPreco(params: any, filial: number, perfil: number): Observable<HttpEvent<ItemResponsePreco[]>> {
+    return this.http.get<ItemResponsePreco[]>
       // tslint:disable-next-line: max-line-length
       (`${this.parameterService.getValueFromProperties(Constantes.GRUPO_DIMED_SERVICE)}mostruario/v3/itens/precos?filial=${filial}&perfil=${perfil}`,
         params);
