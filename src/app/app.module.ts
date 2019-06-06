71fbf386d946cdd51a7497467677c66a64e2bf4d
Alex Siqueira de Freitas <asfreitas@desenvvar26-lnx.dimed.local>
2019-06-06 14:25:54 -0300

    Removed formsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { BootstrapModule } from './modules/bootstrap.module';
 import { InterceptorsModule } from './modules/interceptors.module';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,8 +20,7 @@ import { FormsModule } from '@angular/forms';
     HttpClientModule,
     BootstrapModule,
     BrowserAnimationsModule,
-    InterceptorsModule,
-    FormsModule
+    InterceptorsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
